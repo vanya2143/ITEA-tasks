a1a6bdf58bcd5d29da2c53df1746b6b4c96ad163
Ivan Bielozorov <vanya2143@gmail.com>
2020-05-24 14:39:45 +0300

    Home work 7

---
+++ hw-7/task_1.py
@@ -0,0 +1,63 @@
+# Сделать скрипт, который будет делать GET запросы на следующие ресурсы:
+#     "http://docs.python-requests.org/",
+#     "https://httpbin.org/get",
+#     "https://httpbin.org/",
+#     "https://api.github.com/",
+#     "https://example.com/",
+#     "https://www.python.org/",
+#     "https://www.google.com.ua/",
+#     "https://regex101.com/",
+#     "https://docs.python.org/3/this-url-will-404.html",
+#     "https://www.nytimes.com/guides/",
+#     "https://www.mediamatters.org/",
+#     "https://1.1.1.1/",
+#     "https://www.politico.com/tipsheets/morning-money",
+#     "https://www.bloomberg.com/markets/economics",
+#     "https://www.ietf.org/rfc/rfc2616.txt"
+#
+# Для каждого запроса должен быть вывод по примеру: "Resource 'google.com.ua',
+# request took 0.23 sec, response status - 200."
+# В реализации нет ограничений - можно использовать процессы, потоки, асинхронность.
+# Любые вспомагательные механизмы типа Lock, Semaphore, пулы для тредов и потоков.
+
+
+import aiohttp
+import asyncio
+from time import time
+
+
+async def req(url):
+    session = aiohttp.ClientSession()
+    time_start = time()
+    async with session.get(url) as resp:
+        time_end = time()
+        print(f"Resource '{url}', request took {time_end - time_start:.3f}, response status - {resp.status}")
+    await session.close()
+
+
+if __name__ == '__main__':
+    urls = [
+        "http://docs.python-requests.org/",
+        "https://httpbin.org/get",
+        "https://httpbin.org/",
+        "https://api.github.com/",
+        "https://example.com/",
+        "https://www.python.org/",
+        "https://www.google.com.ua/",
+        "https://regex101.com/",
+        "https://docs.python.org/3/this-url-will-404.html",
+        "https://www.nytimes.com/guides/",
+        "https://www.mediamatters.org/",
+        "https://1.1.1.1/",
+        "https://www.politico.com/tipsheets/morning-money",
+        "https://www.bloomberg.com/markets/economics",
+        "https://www.ietf.org/rfc/rfc2616.txt"
+    ]
+
+    futures = [req(url) for url in urls]
+
+    loop = asyncio.get_event_loop()
+    t_start = time()
+    loop.run_until_complete(asyncio.wait(futures))
+    t_end = time()
+    print(f"Full fetching got {t_end - t_start:.3f} seconds.")
