0840b561afbe81c28920eaf57300e2f7f6134676
Ivan Bielozorov <vanya2143@gmail.com>
2020-08-24 11:09:35 +0300

    Update task_1.py

--- hw-2/task_1.py
+++ hw-2/task_1.py
@@ -4,17 +4,7 @@
 # Пример: get_sum_of_components(123) -> 6 (1+2+3)
 
 def get_sum_of_components_two(n):
-    res = 0
-
-    def my_func(num):
-        nonlocal res
-        num = int(num)
-        if num != 0:
-            res += (num % 10)
-            return my_func(num / 10)
-
-    my_func(n)
-    return res
+    return 0 if not n else n % 10 + get_sum_of_components_two(n // 10)
 
 
 if __name__ == '__main__':
